672c563b1088fde18edade2edd81760537b02a25
alsofer <engenharia@alsofer.com>
2021-08-06 14:25:32 -0300

    retirando tags

--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
-variable "tags" {
-  type = object({
-    Product         = string
-    Team            = string
-  })
-  default           = {
-    Product         = comentarios-api
-    Team            = alsofer-desenvolvimento
-  }
-}
-
 variable "s3_bucket_name" {
     type            = string
     default         = "comentarios-api"
