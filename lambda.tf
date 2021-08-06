2554de7a0787d6f4930bb4994da950d31d26eff0
alsofer <engenharia@alsofer.com>
2021-08-06 15:02:17 -0300

    subindo lambda

---
+++ lambda.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "main" {
+  function_name       = "comentarios-api"
+  role                = aws_iam_role.iam_for_lambda.arn
+  handler             = "main.handler"
+
+
+  runtime = "python3.8"
+
+  environment {
+    variables = {
+      Team = "alsofer"
+    }
+  }
+}
