2da9dbf6074b3728f9e7f8dda6d350678accbfc9
alsofer <engenharia@alsofer.com>
2021-08-07 18:10:39 -0300

    trocando rds

--- rds.tf
+++ rds.tf
@@ -1,57 +1,21 @@
-module "db" {
-  source  = "terraform-aws-modules/rds/aws"
-  version = "~> 3.0"
-
-  identifier = "comentariosmysql"
-
-  engine            = "mysql"
-  engine_version    = "5.7.19"
-  instance_class    = "db.t2.micro"
-  allocated_storage = 5
-
-  name     = "comentariosmysql"
-  username = local.db_creds.username
-  password = local.db_creds.password
-  port     = "3306"
-
-  iam_database_authentication_enabled = false
-
-  vpc_security_group_ids = [data.aws_security_group.main.id]
-
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
-
-  monitoring_interval = "30"
-  monitoring_role_name = "MyRDSMonitoringRole"
-  create_monitoring_role = true
-  publicly_accessible    = true
+module "rds" {
+  source                          = "github.com/skyscrapers/terraform-rds//rds"
+  vpc_id                          = aws_vpc.main.id
+  subnets                         = [aws_subnet.az-a.id, aws_subnet.az-b.id]
+  project                         = "comentarios-api"
+  environment                     = "production"
+  size                            = "db.t2.micro"
+  security_groups                 = [data.aws_security_group.main.id]
+  enabled_cloudwatch_logs_exports = ["audit", "error", "slowquery"]
+  security_groups_count           = 2
+  rds_password                    = local.db_creds.password
+  rds_username                    = local.db_creds.rds_username
+  engine                          = "mysql"
+  engine_version                  = "5.7.19"
+  allowed_cidr_blocks             = ["0.0.0.0/0"]
 
 
-  # DB subnet group
-  subnet_ids = [aws_subnet.az-a.id, aws_subnet.az-b.id]
-
-  # DB parameter group
-  family = "mysql5.7"
-
-  # DB option group
-  major_engine_version = "5.7"
-
-  options = [
-    {
-      option_name = "MARIADB_AUDIT_PLUGIN"
-
-      option_settings = [
-        {
-          name  = "SERVER_AUDIT_EVENTS"
-          value = "CONNECT"
-        },
-        {
-          name  = "SERVER_AUDIT_FILE_ROTATIONS"
-          value = "37"
-        },
-      ]
-    },
-  ]
+  multi_az                        = "false"
 }
 
 data "aws_secretsmanager_secret_version" "creds" {
