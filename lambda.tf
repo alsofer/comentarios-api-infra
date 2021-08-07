31ba7483b4f7be84553f001400a9c2bfe17844cd
alsofer <engenharia@alsofer.com>
2021-08-07 15:07:25 -0300

    attachando lambda à subnet

--- lambda.tf
+++ lambda.tf
@@ -18,6 +18,11 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_lambda_vpc_access_execution" {
+  role       = aws_iam_role.iam_role_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
 resource "aws_lambda_function" "main" {
   function_name       = "comentarios-api"
   role                = aws_iam_role.iam_for_lambda.arn
@@ -25,4 +30,10 @@ resource "aws_lambda_function" "main" {
   s3_bucket           = "comentarios-api"
   s3_key              = "api.zip"
   runtime             = "python3.8"
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.az-a.id]
+    security_group_ids = [aws_security_group.main.id]
+  }
 }
+
