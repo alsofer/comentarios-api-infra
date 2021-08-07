20781980b0e5f1c2b58814e425ae6f1e86cb2cf2
alsofer <engenharia@alsofer.com>
2021-08-07 14:00:25 -0300

    ajustando referencias do api gateway

--- api-gateway.tf
+++ api-gateway.tf
@@ -16,7 +16,7 @@ resource "aws_api_gateway_method" "proxyMethod" {
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   rest_api_id = aws_api_gateway_rest_api.api.id
    resource_id = aws_api_gateway_method.proxyMethod.resource_id
    http_method = aws_api_gateway_method.proxyMethod.http_method
 
@@ -27,8 +27,8 @@ resource "aws_api_gateway_integration" "lambda" {
 
 
 resource "aws_api_gateway_method" "proxy_root" {
-   rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
-   resource_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
+   rest_api_id   = aws_api_gateway_rest_api.api.id
+   resource_id   = aws_api_gateway_rest_api.api.root_resource_id
    http_method   = "ANY"
    authorization = "NONE"
 }
@@ -63,5 +63,5 @@ resource "aws_lambda_permission" "apigw" {
 
    # The "/*/*" portion grants access from any method on any resource
    # within the API Gateway REST API.
-   source_arn = "${aws_api_gateway_rest_api.apiLambda.execution_arn}/*/*"
+   source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
