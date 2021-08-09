f15b4572f041008aa1803e84d20b6ed8912d19e9
alsofer <engenharia@alsofer.com>
2021-08-09 01:21:09 -0300

    ajustando api gateway

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,184 +1,44 @@
-resource "aws_api_gateway_rest_api" "main" {
-  body = jsonencode({
-
-openapi: 3.0.2
-info:
-  title: Comentários API
-  description: Api de comentários online
-  version: '0.1'
-  x-logo:
-    url: 'https://upload.wikimedia.org/wikipedia/pt/2/22/Logotipo_da_Rede_Globo.png'
-servers:
-  - url: https://phl66scaad.execute-api.us-east-2.amazonaws.com/api
-    description: Production server
-paths:
-  /health:
-    get:
-      summary: Healthcheck
-      operationId: healthcheck_api_health_get
-      responses:
-        '200':
-          description: Successful Response
-          content:
-            application/json:
-              schema: {}
-  /comment/list/1:
-    get:
-      summary: Listar Comentários Sobre O Conteúdo 1
-      operationId: listar_coment_rios_sobre_o_conte_do_1_comment_list_1_get
-      responses:
-        '200':
-          description: Successful Response
-          content:
-            application/json:
-              schema: {}
-  /comment/list/2:
-    get:
-      summary: Listar Comentários Sobre O Conteúdo 2
-      operationId: listar_coment_rios_sobre_o_conte_do_2_comment_list_2_get
-      responses:
-        '200':
-          description: Successful Response
-          content:
-            application/json:
-              schema: {}
-  /comment/new/1:
-    post:
-      summary: Enviar Comentário Sobre O Conteúdo 1
-      operationId: enviar_coment_rio_sobre_o_conte_do_1_comment_new_1_post
-      requestBody:
-        content:
-          application/json:
-            schema:
-              $ref: '#/components/schemas/CreateCommentsSchema'
-        required: true
-      responses:
-        '201':
-          description: Successful Response
-          content:
-            application/json:
-              schema: {}
-        '422':
-          description: Validation Error
-          content:
-            application/json:
-              schema:
-                $ref: '#/components/schemas/HTTPValidationError'
-  /comment/new/2:
-    post:
-      summary: Enviar Comentário Sobre O Conteúdo 2
-      operationId: enviar_coment_rio_sobre_o_conte_do_2_comment_new_2_post
-      requestBody:
-        content:
-          application/json:
-            schema:
-              $ref: '#/components/schemas/CreateCommentsSchema'
-        required: true
-      responses:
-        '201':
-          description: Successful Response
-          content:
-            application/json:
-              schema: {}
-        '422':
-          description: Validation Error
-          content:
-            application/json:
-              schema:
-                $ref: '#/components/schemas/HTTPValidationError'
-components:
-  schemas:
-    CreateCommentsSchema:
-      title: CreateCommentsSchema
-      required:
-        - email
-        - comment
-      type: object
-      properties:
-        email:
-          title: Email
-          type: string
-        comment:
-          title: Comment
-          type: string
-    HTTPValidationError:
-      title: HTTPValidationError
-      type: object
-      properties:
-        detail:
-          title: Detail
-          type: array
-          items:
-            $ref: '#/components/schemas/ValidationError'
-    ValidationError:
-      title: ValidationError
-      required:
-        - loc
-        - msg
-        - type
-      type: object
-      properties:
-        loc:
-          title: Location
-          type: array
-          items:
-            type: string
-        msg:
-          title: Message
-          type: string
-        type:
-          title: Error Type
-          type: string
-  })
-
+# API Gateway
+resource "aws_api_gateway_rest_api" "api" {
   name = "api"
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
 }
 
-resource "aws_api_gateway_deployment" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.main.body))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_api_gateway_stage" "main" {
-  deployment_id = aws_api_gateway_deployment.main.id
-  rest_api_id   = aws_api_gateway_rest_api.main.id
-  stage_name    = "api"
-}
-
-resource "aws_api_gateway_resource" "main" {
+## ROTAS:
+  ###### /health
+resource "aws_api_gateway_resource" "health" {
   path_part   = "health"
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
-
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.main.id
-  resource_id   = aws_api_gateway_resource.main.id
+resource "aws_api_gateway_method" "get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.health.id
   http_method   = "GET"
   authorization = "NONE"
 }
-
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.main.id
-  http_method             = aws_api_gateway_method.method.http_method
+  resource_id             = aws_api_gateway_resource.health.id
+  http_method             = aws_api_gateway_method.get.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.main.invoke_arn
 }
 
-# Lambda
+  ###### /comment/new/1
+
+
+  ###### /comment/new/2
+
+
+  ###### /comment/list/1
+
+
+  ###### /comment/list/2
+
+
+
+# Autorização para lambda
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
