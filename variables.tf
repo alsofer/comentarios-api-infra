113e9fe24c157f82ed0decaf49192f89c72b9727
alsofer <engenharia@alsofer.com>
2021-08-09 01:29:00 -0300

    arrumando policy para execução da lambda

--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "s3_bucket_name" {
     type            = string
     default         = "s3-comentarios-api"
 }
+
+variable "myregion" {
+    default = "us-east-2"
+}
+
+variable "accountId" {
+    default = "107815129133"
+}
