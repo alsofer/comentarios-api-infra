2a89c58066b32b334020b76f057688edf373ef91
alsofer <engenharia@alsofer.com>
2021-08-09 03:42:11 -0300

    add route table rule

--- vpc.tf
+++ vpc.tf
@@ -40,3 +40,14 @@ resource "aws_security_group_rule" "lambda-out" {
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route = [
+    {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.gw.id
+    }
+  ]
+}
