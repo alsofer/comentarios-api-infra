27012636fbd3ac4b7884baacfd000d3c62e76d81
alsofer <engenharia@alsofer.com>
2021-08-09 05:35:12 -0300

    reestruturação da api gateway, criação dos paths finais

--- api-gateway.tf
+++ api-gateway.tf
@@ -5,7 +5,7 @@ resource "aws_api_gateway_rest_api" "api" {
 
 resource "aws_api_gateway_deployment" "api" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-}
+  }
 
 resource "aws_api_gateway_stage" "api" {
   deployment_id = aws_api_gateway_deployment.api.id
@@ -35,18 +35,113 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = aws_lambda_function.main.invoke_arn
 }
 
-  ###### /comment/new/1
+  ###### /comment
+resource "aws_api_gateway_resource" "comment" {
+  path_part   = "comment"
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
 
+  ###### /comment/new
+resource "aws_api_gateway_resource" "commentnew" {
+  path_part   = "new"
+  parent_id   = aws_api_gateway_resource.comment.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
 
-  ###### /comment/new/2
+  ## /1
+resource "aws_api_gateway_resource" "commentnew1" {
+  path_part   = "1"
+  parent_id   = aws_api_gateway_resource.commentnew.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
 
+resource "aws_api_gateway_method" "commentnew1post" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.commentnew1.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_integration" "integrationcommentnew1post" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.commentnew1.id
+  http_method             = aws_api_gateway_method.commentnew1post.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.main.invoke_arn
+}
 
-  ###### /comment/list/1
+  ## /2
+resource "aws_api_gateway_resource" "commentnew2" {
+  path_part   = "2"
+  parent_id   = aws_api_gateway_resource.commentnew.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
 
+resource "aws_api_gateway_method" "commentnew2post" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.commentnew2.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_integration" "integrationcommentnew2post" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.commentnew2.id
+  http_method             = aws_api_gateway_method.commentnew2post.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.main.invoke_arn
+}
 
-  ###### /comment/list/2
+  ###### /comment/list
+resource "aws_api_gateway_resource" "commentlist" {
+  path_part   = "list"
+  parent_id   = aws_api_gateway_resource.comment.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
 
+  ## /1
+resource "aws_api_gateway_resource" "commentlist1" {
+  path_part   = "1"
+  parent_id   = aws_api_gateway_resource.commentlist.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
 
+resource "aws_api_gateway_method" "commentlist1get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.commentlist1.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_integration" "integrationcommentlist1" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.commentlist1.id
+  http_method             = aws_api_gateway_method.commentlist1get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.main.invoke_arn
+}
+  ## /2
+resource "aws_api_gateway_resource" "commentlist2" {
+  path_part   = "2"
+  parent_id   = aws_api_gateway_resource.commentlist.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "commentlist2get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.commentlist2.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_integration" "integrationcommentlist2" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.commentlist2.id
+  http_method             = aws_api_gateway_method.commentlist2get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.main.invoke_arn
+}
 
 # Autorização para lambda
 resource "aws_lambda_permission" "apigw_lambda" {
